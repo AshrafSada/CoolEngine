ae55be03a2985ffcbe022ffbc3c3d80f0f596cfa
Ashraf Sada <ashraf.sada@outlook.com>
2023-10-09 15:55:26 +0300

    - Added Cool Engine Solution - Added Cool Core Static Library

---
+++ Cool/Cool.Core/Cool.Core.cpp
@@ -0,0 +1,10 @@
+// Cool.Core.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnCoolCore()
+{
+}
