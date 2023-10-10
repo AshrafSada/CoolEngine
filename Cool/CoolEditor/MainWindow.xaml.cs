3ca8e4a90a28672090839f435b041a469dd55fc4
Ashraf Sada <ashraf.sada@outlook.com>
2023-10-10 22:07:12 +0300

    - Added Project browser page - Added Buttons Open Create And Delete

---
+++ Cool/CoolEditor/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using CoolEditor.Pages;
+
+namespace CoolEditor
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      Loaded += OnMainWindow_Loaded;
+    }
+
+    private void OnMainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+      Loaded -= OnMainWindow_Loaded;
+      OpenProjectsBrowser();
+    }
+
+    private void OpenProjectsBrowser()
+    {
+      var projectsBrowser = new ProjectsBrowser();
+
+      if (projectsBrowser.ShowDialog() == false)
+      {
+        Application.Current.Shutdown();
+      }
+      else
+      {
+        projectsBrowser.Show();
+      }
+    }
+  }
+}
---
+++ Cool/CoolEditor/Pages/ProjectsBrowser.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace CoolEditor.Pages
+{
+  /// <summary>
+  /// Interaction logic for ProjectsBrowser.xaml
+  /// </summary>
+  public partial class ProjectsBrowser : Window
+  {
+    public ProjectsBrowser()
+    {
+      InitializeComponent();
+    }
+
+    private void BtnOpenProject_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void BtnCreateProject_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void BtnDeleteProject_Click(object sender, RoutedEventArgs e)
+    {
+    }
+  }
+}
